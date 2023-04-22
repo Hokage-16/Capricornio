e7dd07c4725be26ccf4f6fb6081c85f670eb0c4e
Ronaldo <pasoedyronaldo@gmail.com>
2023-04-21 19:47:49 -0500

    se sube el proyecto

---
+++ android/app/src/main/kotlin/com/example/capricornio/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.capricornio
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/pantallas/login.dart
@@ -0,0 +1,54 @@
+import 'package:flutter/material.dart';
+
+import '../widgets/formlogin.dart';
+
+
+class LoginApp extends StatefulWidget {
+  const LoginApp({super.key});
+
+  @override
+  State<LoginApp> createState() => _LoginAppState();
+}
+
+class _LoginAppState extends State<LoginApp> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: const Color.fromARGB(255, 181, 171, 226),
+      body:ListView(
+        padding: const EdgeInsets.symmetric(
+          horizontal: 30.0,
+          vertical: 80.0
+        ),
+        children: <Widget>[
+          Column(
+            mainAxisAlignment: MainAxisAlignment.center,       
+            children:const [
+              CircleAvatar(
+              radius: 100.0,
+              backgroundImage: AssetImage('images/logo.png'),
+         ),
+         Text(
+          'login',
+          style: TextStyle(
+            fontFamily: 'dynapuff',
+            fontSize: 50.0,
+            color: Color.fromARGB(255, 39, 39, 43)
+          ),
+         ),
+        SizedBox(
+          width: 250.0,
+          height: 35.0,
+          child: Divider(
+            color: Color.fromARGB(255, 76, 76, 83)
+          ),
+         ),
+         Formulario()
+            ],
+          )
+        ],
+      ),
+    );
+
+  }
+}
